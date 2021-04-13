863eeda3eb0489c40fbb406d0cd240cacb64549b
Daeun Lee <leede0418@likelion.org>
2021-04-13 21:08:01 +0900

    feat(method-argument): handler 성공

--- src/main/java/nextstep/helloworld/mvc/handler/MethodArgumentController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/MethodArgumentController.java
@@ -9,11 +9,11 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/method-argument")
+@RequestMapping("/method-argument/users")
 public class MethodArgumentController {
 
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> requestParam(String userName) {
+    @GetMapping
+    public ResponseEntity<List<User>> requestParam(@RequestParam("name") String userName) {
         List<User> users = Arrays.asList(
                 new User(userName, "email"),
                 new User(userName, "email")
@@ -21,8 +21,8 @@ public class MethodArgumentController {
         return ResponseEntity.ok().body(users);
     }
 
-    @PostMapping("/users/body")
-    public ResponseEntity requestBody(User user) {
+    @PostMapping("/body")
+    public ResponseEntity requestBody(@RequestBody User user) {
         User newUser = new User(1L, user.getName(), user.getEmail());
         return ResponseEntity.created(URI.create("/users/" + newUser.getId())).body(newUser);
     }
