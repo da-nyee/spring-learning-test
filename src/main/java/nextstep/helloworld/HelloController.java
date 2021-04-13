fb1e27b94d945d52423f981ef2260180540788ac
Daeun Lee <leede0418@likelion.org>
2021-04-13 14:55:57 +0900

    feat(controller): Controller에 Dao 의존성 주입
    - 의존성 주입은 생성자 주입 방법 사용

--- src/main/java/nextstep/helloworld/HelloController.java
+++ src/main/java/nextstep/helloworld/HelloController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private HelloDao helloDao;
+
+    public HelloController(HelloDao helloDao) {
+        this.helloDao = helloDao;
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "") String name) {
         return "Hello " + name;
